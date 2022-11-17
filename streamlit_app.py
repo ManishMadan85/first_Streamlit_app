2e7f62d3b0ec335465e97489c7367505ec074db1
ManishMadan85 <85575709+ManishMadan85@users.noreply.github.com>
2022-11-17 14:40:47 +0530

    Add_Dataset

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
+import pandas as pd
 streamlit.title("My parents New health dinner")
 streamlit.header("🥗 🐔 🥑🍞 Breakfast Menu 🥗 🐔 🥑🍞")
 streamlit.text("Omega 3 sandwich 🍞")
 streamlit.text("Spanich,Rice 🥣")
 streamlit.text("Brown Bread 🥗 🍞")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+My_fruits_List=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(My_fruits_List)
