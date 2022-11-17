3e07971947dbfe3118bfb9b2487564aae96dfc2c
ManishMadan85 <85575709+ManishMadan85@users.noreply.github.com>
2022-11-17 14:33:52 +0530

    Update streamlit_app.py
    New Upadate

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title("My parents New health dinner")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 sandwich")
+streamlit.text("Spanich,Rice")
+streamlit.text("Brown Bread")
