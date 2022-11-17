3576404ec6b6c5c601c888e9ab9ec3dce6c1c85b
ManishMadan85 <85575709+ManishMadan85@users.noreply.github.com>
2022-11-17 14:36:01 +0530

    Update streamlit_app.py
    Update 2.1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title("My parents New health dinner")
-streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 sandwich")
-streamlit.text("Spanich,Rice")
-streamlit.text("Brown Bread")
+streamlit.header("🥗 🐔 🥑🍞 Breakfast Menu 🥗 🐔 🥑🍞")
+streamlit.text("Omega 3 sandwich 🍞")
+streamlit.text("Spanich,Rice 🥣")
+streamlit.text("Brown Bread 🥗 🍞")
